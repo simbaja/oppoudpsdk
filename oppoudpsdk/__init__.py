da322cf7d75e82d2aa1bdf7b7b7fc3a220ad3497
Jack Simbach <jack.simbach@gmail.com>
2021-07-06 11:58:19 -0400

    - fixed import - version bump

--- oppoudpsdk/__init__.py
+++ oppoudpsdk/__init__.py
@@ -1,10 +1,11 @@
 """Oppo UDP SDK"""
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 from .codes import OppoRemoteCode, OppoRemoteCodeType
 from .const import *
 from .exceptions import *
 from .command import *
+from .response.enums import *
 from .client import OppoClient
 from .device import OppoDevice
